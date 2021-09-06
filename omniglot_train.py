48f9bcb1418d62509534b801265e829cf4884325
Xin Huang <57054352+isHuangXin@users.noreply.github.com>
2021-09-06 16:32:35 +0800

    update imniglot_train

--- omniglot_train.py
+++ omniglot_train.py
@@ -41,7 +41,28 @@ def main(args):
                              k_shot=args.k_spt,
                              k_query=args.k_qry,
                              imgsz=args.imgsz)
-
+    for step in range(args.epoch):
+        x_spt, y_spt, x_qry, y_qry = db_train.next()
+        x_spt, y_spt, x_qry, y_qry = torch.from_numpy(x_spt).to(device), torch.from_numpy(y_spt).to(device),\
+                                     torch.from_numpy(x_qry).to(device), torch.from_numpy(y_qry).to(device)
+        # set traning = True to update running_mean, running_variance, bn_weights, bn_bias
+        accs = maml(x_spt, y_spt, x_qry, y_qry)
+        if step % 50 == 0:
+            print('step:', step, '\ttraining acc:', accs)
+        if step % 500 == 0:
+            accs = []
+            for _ in range(100//args.task_num):
+                # test
+                x_spt, y_spt, x_qry, y_qry = db_train.next('test')
+                x_spt, y_spt, x_qry, y_qry = torch.from_numpy(x_spt).to(device), torch.from_numpy(y_spt).to(device),\
+                                             torch.from_numpy(x_qry).to(device), torch.from_numpy(y_qry).to(device)
+                # spilt to single task each time
+                for x_spt_one, y_spt_one, x_qry_one, y_qry_one in zip(x_spt, y_spt, x_qry, y_qry):
+                    test_acc = maml.finetunning(x_spt_one, y_spt_one, x_qry_one, y_qry_one)
+                    accs.append(test_acc)
+            # [b, update_step+1]
+            accs = np.array(accs).mean(axis=0).astype(np.float16)
+            print('Test acc:', accs)
 
 
 if __name__ == '__main__':
